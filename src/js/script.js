8622025bb37cb5c41cb256b3d1f1f83812be30bb
Kamil Musialik <kamilmusialik22@gmail.com>
2022-03-21 13:03:18 +0100

    add const payload in sendOrder

--- src/js/script.js
+++ src/js/script.js
@@ -71,6 +71,11 @@
     cart: {
       defaultDeliveryFee: 20,
     },
+    db: {
+      url: '//localhost:3131',
+      products: 'products',
+      orders: 'orders',
+    },
   };
 
   const templates = {
@@ -360,6 +365,9 @@
       thisCart.dom.subtotalPrice = thisCart.dom.wrapper.querySelector(select.cart.subtotalPrice);
       thisCart.dom.totalPrice = thisCart.dom.wrapper.querySelectorAll(select.cart.totalPrice);
       thisCart.dom.totalNumber = thisCart.dom.wrapper.querySelector(select.cart.totalNumber);
+      thisCart.dom.form = thisCart.dom.wrapper.querySelector(select.cart.form);
+      thisCart.dom.phone = thisCart.dom.wwrapper.querySelector(select.cart.phone);
+      thisCart.dom.address = thisCart.dom.wrapper.querySelector(select.cart.address);
     }
 
     initActions(){
@@ -375,6 +383,11 @@
 
       thisCart.dom.productList.addEventListener('remove', function(event){
         thisCart.remove(event);
+      });
+
+      thisCart.dom.form.addEventListener('submit', function(event){
+        event.preventDefault();
+        thisCart.sendOrder();
       });
     }
 
@@ -429,6 +442,22 @@
       thisCart.products.splice(thisCartProductRemove, 1);
 
       thisCart.update();
+    }
+
+    sendOrder(){
+      const thisCart = this;
+
+      const url = settings.db.url + '/' + settings.db.orders;
+
+      const payload = {
+        address: thisCart.dom.address.value,
+        phone: thisCart.dom.phone.value,
+        totalPrice: thisCart.totalPrice,
+        subtotalPrice: thisCart.subtotalPrice,
+        totalNumber: thisCart.totalNumber,
+        deliveryFee: thisCart.defaultDeliveryFee,
+        //products: ,
+      };
     }
   }
 
@@ -501,17 +530,34 @@
   }
 
   const app = {
-    initData: function(){
-      const thisApp = this;
-
-      thisApp.data = dataSource;
-    },
 
     initMenu: function(){
       const thisApp = this;
       for(let productData in thisApp.data.products){
-        new Product(productData, thisApp.data.products[productData]);
+        new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
       }
+    },
+
+    initData: function(){
+      const thisApp = this;
+      const url = settings.db.url + '/' + settings.db.products;
+
+      thisApp.data = {};
+
+      fetch(url)
+        .then(function(rawResonse){
+          return rawResonse.json();
+        })
+        .then(function(parsedResponse){
+          console.log('parsedResponse', parsedResponse);
+
+          //save parsedResponse as thisApp.data.products
+          thisApp.data.products = parsedResponse;
+
+          //execute initMenu method
+          thisApp.initMenu();
+        });
+      console.log('thisapp.data', JSON.stringify(thisApp.data));
     },
 
     initCart: function(){
@@ -525,7 +571,6 @@
       const thisApp = this;
 
       thisApp.initData();
-      thisApp.initMenu();
       thisApp.initCart();
     },
   };
