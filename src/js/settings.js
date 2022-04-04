621ce26990e3f4fd58a9b7258ed225b26da17113
Kamil Musialik <kamilmusialik22@gmail.com>
2022-04-04 12:31:14 +0200

    add HomePage class and Handlebar

--- src/js/settings.js
+++ src/js/settings.js
@@ -3,6 +3,7 @@ export const select = {
     menuProduct: '#template-menu-product',
     cartProduct: '#template-cart-product',
     bookingWidget: '#template-booking-widget',
+    homeWidget: 'template-home-page',
   },
   containerOf: {
     menu: '#product-list',
@@ -129,4 +130,5 @@ export const templates = {
   menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
   cartProduct: Handlebars.compile(document.querySelector(select.templateOf.cartProduct).innerHTML),
   bookingWidget: Handlebars.compile(document.querySelector(select.templateOf.bookingWidget).innerHTML),
+  homeWidget: Handlebars.compile(document.querySelector(select.containerOf.homeWidget).innerHTML),
 };
